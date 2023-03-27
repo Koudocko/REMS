993010b4253d9b271859c0d73248f7647c776244
Koudocko <tyler@pcquest.ca>
2023-03-27 15:17:12 -0400

    REMS2 logging now uses json communication format.

--- log.py
+++ log.py
@@ -1,3 +1,4 @@
+import json
 import time
 import stat
 import os
@@ -30,27 +31,28 @@ sys.stdout.write('\n')
 
 def sgp30_log():
     result = sgp30.get_air_quality()
-    return "SGP30", "C02: " + str(result.equivalent_co2) + ", TVOC: " + str(result.total_voc)
+    return "SGP30", { "C02": + result.equivalent_co2, "TVOC": result.total_voc }
 
 def mics_log():
     result = gas.read_all()
-    return "MICS6814", "Oxidising: " + str(result.oxidising) + ", Reducing: " + str(result.reducing) + ", NH3: " + str(result.nh3) + ", ADC: " + str(result.adc)
+    return "MICS6814", { "Oxidising": result.oxidising, "Reducing": result.reducing, "NH3": result.nh3, "ADC": result.adc }
 
 def bme280_log():
-    return "BME280", "Temperature: " + str(bme280.get_temperature()) + ", Pressure: " + str(bme280.get_pressure()) + ", Humidity: " + str(bme280.get_humidity())
+    return "BME280", { "Temperature": bme280.get_temperature(), "Pressure": bme280.get_pressure(), "Humidity": bme280.get_humidity() }
 
 sensors = [sgp30_log, mics_log, bme280_log]
+log_data = { "data": {}, "time": datetime.now().strftime("%H:%M:%S") }
 
 file_handle = open(log_path, "w")
-
 file_handle.seek(0)
+
 for sensor in sensors:
    res = sensor()
-   data_time = datetime.now().strftime("%H:%M:%S")
+   log_data["data"][res[0]] = res[1]
 
-   file_handle.write("MODULE: " + res[0] + " - READ: " + res[1] + " - TIME: " + data_time + "\n")
-   file_handle.flush()
+file_handle.write(json.dumps(log_data))
+file_handle.flush()
 file_handle.truncate()
-os.system("/rems/commands/rems_2_transfer_sh")
-
 file_handle.close()
+
+os.system("/rems/commands/rems_2_transfer_sh")
