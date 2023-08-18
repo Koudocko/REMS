bb62a962b83c3b778581a2dcd1e8c9a990182cc3
Koudocko <tyler@pcquest.ca>
2023-08-18 17:54:15 -0400

    Fix.

--- weather/weather_build.sh
+++ weather/weather_build.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+# APT packages
+sudo apt-get update
+sudo apt-get install python3 python3-pip apache2 libapache2-mod-wsgi-py3
+
+# Django
+pip install django
+django-admin
+
 # Breakout garden
-sudo apt-get install python3
 wget https://github.com/pimoroni/breakout-garden/archive/master.zip
 unzip master.zip
 sudo ./breakout-garden-master/install.sh
@@ -19,6 +26,12 @@ sudo rm cron_temp
 sudo mkdir -p /rems/readings
 sudo mkdir -p /rems/commands
 sudo touch /rems/readings/weather.txt
-sudo chmod -R 755 /rems/readings
+sudo chmod -R 777 /rems/readings
 sudo cp weather.py /rems/commands/weather.py
 sudo cp transfer.sh /rems/commands/transfer.sh
+
+# Transfer
+read -p "Host (IP):" HOST
+read -p "User:" USER
+read -p "Password:" PASS
+echo -e "HOST=$HOST\nUSER=$USER\nPASS:$PASS" > .env
