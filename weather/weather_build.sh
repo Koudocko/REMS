197766cbe7a278d9208ce10efdaeefbd3fba13c6
Koudocko <tyler@pcquest.ca>
2023-08-22 13:06:41 -0400

    Fixed pimoroni docker build.

--- weather/weather_build.sh
+++ weather/weather_build.sh
@@ -41,12 +41,16 @@ install_webserver(){
       weather-webserver
 }
 
+# APT depency check
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+
 # Add docker repo to keyring
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpgsudo 
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/raspbian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
